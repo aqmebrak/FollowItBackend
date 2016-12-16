d6b6702af103a7ce7bc02930df8f50a6125a95a5
aqmebrak <aqmebrak@gmail.com>
2016-12-16 10:29:18 +0100

    added base files

---
+++ app/index.js
@@ -0,0 +1,72 @@
+// server.js
+
+// PACKAGES
+// ===============================================================================
+
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+// ===============================================================================
+
+// DB CONNECT
+// =============================================================================
+var db = mongoose.connect('mongodb://admin:Catchouball06@ds135798.mlab.com:35798/followit', function (err) {
+    if (err) console.log(err);
+});
+// =============================================================================
+
+
+// ROUTING CONFIG
+// =============================================================================
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    console.log(req.body);
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
+});
+
+// more routes for our API will happen here
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+// create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function (req, res) {
+        console.log("bear post");
+
+    })
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function (req, res) {
+        Bear.find(function (err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+// Register our routes : all of our routes will be prefixed with /api
+app.use('/api', router);
+// =============================================================================
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
