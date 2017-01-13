c9243ef816d235ebc5f4e9579f2df2dbbea06b71
aqmebrak <aqmebrak@gmail.com>
2017-01-13 17:12:28 +0100

    added CORS

--- index.js
+++ index.js
@@ -58,6 +58,11 @@ io.on('connection', function (socket) {
 // =============================================================================
 // configure app to use bodyParser()
 // this will let us get the data from a POST
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
